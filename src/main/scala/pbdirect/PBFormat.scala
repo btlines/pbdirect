baf367b5fb6c232a853c18898443a2a8c3977705
damien <damien@beyondthelines.net>
2017-11-01 09:21:11 +0000

    Add extensibility support for custom types
    - Add PBFormat a combination of PBReader and PBWriter
    - Use cats to provide extensibility methods to support custom types:
      - provide map to create new PBReader from existing ones
      - provide contramap to create new PBWriter from existing ones
      - provide imap to create new PBFormat from existing ones

---
+++ src/main/scala/pbdirect/PBFormat.scala
@@ -0,0 +1,27 @@
+package pbdirect
+
+import cats.Functor
+import cats.functor.{Contravariant, Invariant}
+import com.google.protobuf.CodedOutputStream
+
+sealed trait PBFormat[A] extends PBReader[A] with PBWriter[A]
+
+trait PBFormatImplicits {
+  implicit object InvariantFormat extends Invariant[PBFormat] {
+    override def imap[A, B](format: PBFormat[A])(f: A => B)(g: B => A): PBFormat[B] =
+      PBFormat[B](
+        Functor[PBReader].map(format)(f),
+        Contravariant[PBWriter].contramap(format)(g)
+      )
+  }
+}
+
+object PBFormat extends PBFormatImplicits {
+  def apply[A](implicit reader: PBReader[A], writer: PBWriter[A]): PBFormat[A] =
+    new PBFormat[A] {
+      override def read(index: Int, bytes: Array[Byte]): A =
+        reader.read(index, bytes)
+      override def writeTo(index: Int, value: A, out: CodedOutputStream): Unit =
+        writer.writeTo(index, value, out)
+    }
+}
