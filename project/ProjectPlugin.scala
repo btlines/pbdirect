31164c9feb9125ea595972a1684d6e9c4109b502
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-21 08:23:36 +0100

    Update scalatest to 3.1.1 (#51)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -24,7 +24,7 @@ object ProjectPlugin extends AutoPlugin {
       val scala213: String            = "2.13.1"
       val shapeless: String           = "2.3.3"
       val enumeratum: String          = "1.5.15"
-      val scalaTest: String           = "3.1.0"
+      val scalaTest: String           = "3.1.1"
       val scalatestScalacheck: String = "3.1.0.1"
       val scalacheckShapeless: String = "1.2.4"
       val protocJar: String           = "3.11.1"
