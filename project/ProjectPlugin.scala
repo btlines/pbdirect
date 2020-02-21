38869acc17c5322020a120cbe3f53f40ffa46053
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-02-21 12:18:20 +0100

    Update scalacheck-1-14 to 3.1.1.1 (#55)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -25,7 +25,7 @@ object ProjectPlugin extends AutoPlugin {
       val shapeless: String           = "2.3.3"
       val enumeratum: String          = "1.5.15"
       val scalaTest: String           = "3.1.1"
-      val scalatestScalacheck: String = "3.1.1.0"
+      val scalatestScalacheck: String = "3.1.1.1"
       val scalacheckShapeless: String = "1.2.4"
       val protocJar: String           = "3.11.1"
     }
