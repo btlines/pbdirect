5021ea93f715d14a63fd2cc8107e0d495a97eb54
Alejandro Hernández <info@alejandrohdezma.com>
2020-06-02 08:35:19 +0200

    We can inline dependency versions since they're going to be updated by Scala Steward

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -9,33 +9,17 @@ object ProjectPlugin extends AutoPlugin {
 
   override def trigger: PluginTrigger = allRequirements
 
-  object autoImport {
-
-    lazy val V = new {
-      val cats: String                = "2.1.1"
-      val protobuf: String            = "3.12.2"
-      val shapeless: String           = "2.3.3"
-      val enumeratum: String          = "1.6.1"
-      val scalaTest: String           = "3.1.2"
-      val scalatestScalacheck: String = "3.1.2.0"
-      val scalacheckShapeless: String = "1.2.5"
-      val protocJar: String           = "3.11.4"
-    }
-  }
-
-  import autoImport._
-
   override def projectSettings: Seq[Def.Setting[_]] =
     Seq(
       libraryDependencies ++= Seq(
-        "com.chuusai"                %% "shapeless"                 % V.shapeless,
-        "org.typelevel"              %% "cats-core"                 % V.cats,
-        "com.google.protobuf"         % "protobuf-java"             % V.protobuf,
-        "com.beachape"               %% "enumeratum"                % V.enumeratum,
-        "org.scalatest"              %% "scalatest"                 % V.scalaTest           % Test,
-        "org.scalatestplus"          %% "scalacheck-1-14"           % V.scalatestScalacheck % Test,
-        "com.github.alexarchambault" %% "scalacheck-shapeless_1.14" % V.scalacheckShapeless % Test,
-        "com.github.os72"             % "protoc-jar"                % V.protocJar           % Test
+        "com.chuusai"                %% "shapeless"                 % "2.3.3",
+        "org.typelevel"              %% "cats-core"                 % "2.1.1",
+        "com.google.protobuf"         % "protobuf-java"             % "3.12.2",
+        "com.beachape"               %% "enumeratum"                % "1.6.1",
+        "org.scalatest"              %% "scalatest"                 % "3.1.2" % Test,
+        "org.scalatestplus"          %% "scalacheck-1-14"           % "3.1.2.0" % Test,
+        "com.github.alexarchambault" %% "scalacheck-shapeless_1.14" % "1.2.5" % Test,
+        "com.github.os72"             % "protoc-jar"                % "3.11.4" % Test
       ),
       Compile / scalacOptions -= "-Xfatal-warnings",
       Compile / console / scalacOptions -= "-Xlint"
